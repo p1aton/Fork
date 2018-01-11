7f1843b6b68fc257d7a390c07132e8ddd96c7399
ivigamberdiev <ivigamberdiev@gmail.com>
2018-01-11 20:57:00 +0300

    fix timeout bug and some minor bugfixes

--- Exchange/cex.py
+++ Exchange/cex.py
@@ -4,14 +4,12 @@ import psycopg2.extras
 import time
 conn_string = "dbname='igor' user='igor' password='Chordify2811' host='138.197.179.83'"
 while True:
-    start = time.time()
-    cex_data1 = requests.get('https://cex.io/api/tickers/BTC/USD',timeout=1)
-    cex_data2 = requests.get('https://cex.io/api/tickers/EUR/GBP',timeout=1)
-    cex_data3 = requests.get('https://cex.io/api/tickers/BTC/RUB',timeout=1)
-
-    
+    start = time.time()  
     try:
-        conn = psycopg2.connect(conn_string) # пишем таймстемп
+        cex_data1 = requests.get('https://cex.io/api/tickers/BTC/USD',timeout=1)
+        cex_data2 = requests.get('https://cex.io/api/tickers/EUR/GBP',timeout=1)
+        cex_data3 = requests.get('https://cex.io/api/tickers/BTC/RUB',timeout=1)
+        conn = psycopg2.connect(conn_string)
         cur = conn.cursor()
         cur.execute("INSERT INTO cex_ts(ts) VALUES (CURRENT_TIMESTAMP);")
         cur.execute("SELECT CURRVAL('cex_ts_id_seq');")
@@ -21,7 +19,6 @@ while True:
         conn.close()
 
         cex_answer = 1
-
         cex_nums = []
         cex_values = []
         for i in range(0,len(cex_data1.json()['data'])):
@@ -39,37 +36,25 @@ while True:
             if cex_nums[i][0:4] == 'dash':
                 b = cex_nums[i][4:8]
                 cex_nums[i] = 'dsh'+b
-        cex = []
-        for i in range(0,len(cex_nums)):
-            cex.append((cex_nums[i],cex_values[i], cex_id[0]))
+    
+    except requests.exceptions.Timeout:            
         conn = psycopg2.connect(conn_string)
         cur = conn.cursor()
-        psycopg2.extras.execute_values(cur, "INSERT INTO cex(br, value, idt) values %s", cex)
+        cur.execute("INSERT INTO cex_ts(ts) VALUES (CURRENT_TIMESTAMP);")
+        cur.execute("SELECT CURRVAL('cex_ts_id_seq');")
+        cex_id = cur.fetchone()
         conn.commit()
         cur.close()
         conn.close()
-    
-        except requests.exceptions.Timeout:
-            
-            conn = psycopg2.connect(conn_string) # пишем таймстемп
-            cur = conn.cursor()
-            cur.execute("INSERT INTO cex_ts(ts) VALUES (CURRENT_TIMESTAMP);")
-            cur.execute("SELECT CURRVAL('cex_ts_id_seq');")
-            cex_id = cur.fetchone()
-            conn.commit()
-            cur.close()
-            conn.close()
-            
-            cex_answer = 0
-            
-            cex = []
-            for i in range(0,len(cex_nums)):
-                cex.append((cex_nums[i],cex_values[i], cex_id[0]))
-            conn = psycopg2.connect(conn_string)
-            cur = conn.cursor()
-            psycopg2.extras.execute_values(cur, "INSERT INTO cex(br, value, idt) values %s", cex)
-            conn.commit()
-            cur.close()
-            conn.close()
+
+        cex_answer = 0
+    cex = []
+    for i in range(0,len(cex_nums)):
+        cex.append((cex_nums[i],cex_values[i], cex_id[0]))
+    conn = psycopg2.connect(conn_string)
+    cur = conn.cursor()
+    psycopg2.extras.execute_values(cur, "INSERT INTO cex(br, value, idt) values %s", cex)
+    conn.commit()
+    cur.close()
+    conn.close()
     time.sleep(10-(time.time() - start))
-            
