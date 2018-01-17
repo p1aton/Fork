bc3dda86d71394186f0212a929fc60392271514a
ivigamberdiev <ivigamberdiev@gmail.com>
2018-01-17 22:24:42 +0300

    Update botdb.py

--- botdb.py
+++ botdb.py
@@ -1,7 +1,10 @@
 import psycopg2
-conn = psycopg2.connect(dbname="igor", user="bot", password="Chordify2811", host="138.197.179.83")
+conn_string = "dbname='igor' user='igor' password='Chordify2811' host='138.197.179.83'"
+conn = psycopg2.connect(conn_string)
 cur = conn.cursor()
-cur.execute("SELECT br, value FROM pairs ORDER BY idt DESC LIMIT 14;")
-profit = cur.fetchall()
+cur.execute("SELECT id FROM pairs_ts ORDER BY id DESC LIMIT 1;")
+idt = cur.fetchone()
+cur.execute("SELECT * FROM pairs WHERE idt=%s", idt)
+data = cur.fetchall()
 cur.close()
 conn.close()
