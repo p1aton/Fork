5ab5c1d94aa6dacd615ce04bd307af2b11115598
ivigamberdiev <ivigamberdiev@gmail.com>
2018-01-21 15:15:29 +0300

    with commisions

--- usdt_parsing.py
+++ usdt_parsing.py
@@ -2,31 +2,26 @@ import requests
 import time
 import psycopg2
 import crypto_parsing
-conn_string = "dbname='igor'"
+conn_string = "dbname='igor' user='igor' password='Chordify2811' host='138.197.179.83'"
 
 while True:
     start = time.time()
     try:
         r = requests.get('https://api.coinmarketcap.com/v1/ticker/',timeout=3).json()
         com_usd,com_btc,com_eth,com_eur,com_rub = crypto_parsing.get_all_comissions()
-        
-        
         usdt_usd = r[22]['price_usd']
         usdt_answer = 1
         com_answer = 1
         
     except Exception:
-        
         usdt_answer = 0
         com_answer = 0
-    
-    
     finally:
         conn = psycopg2.connect(conn_string)
         cur = conn.cursor()
-        cur.execute("INSERT INTO usdt(value, ts) VALUES (%s, CURRENT_TIMESTAMP);", [usdt_usd])
+        cur.execute("""INSERT INTO usdt_com(usdt, com_usd, com_btc, com_eth, com_eur, com_rub, ts)
+                    VALUES (%s, %s, %s, %s, %s, %s, CURRENT_TIMESTAMP);""", (float(usdt_usd), str(com_usd), str(com_btc), str(com_eth), str(com_eur), str(com_rub)))
         conn.commit()
         cur.close()
         conn.close()
     time.sleep(60 - (time.time() - start))
-        
