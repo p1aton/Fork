d07950944cf3f1aa9d90a06d1c76c8246513f3d5
ilya <ilya1242@gmail.com>
2018-01-21 14:48:32 +0300

    usdt-parse

--- usdt_parsing.py
+++ usdt_parsing.py
@@ -1,19 +1,25 @@
 import requests
 import time
 import psycopg2
+import crypto_parsing
 conn_string = "dbname='igor'"
 
 while True:
     start = time.time()
     try:
         r = requests.get('https://api.coinmarketcap.com/v1/ticker/',timeout=3).json()
+        com_usd,com_btc,com_eth,com_eur,com_rub = crypto_parsing.get_all_comissions()
+        
         
         usdt_usd = r[22]['price_usd']
         usdt_answer = 1
+        com_answer = 1
         
-    except requests.exceptions.RequestException:
+    except Exception:
         
         usdt_answer = 0
+        com_answer = 0
+    
     
     finally:
         conn = psycopg2.connect(conn_string)
@@ -22,5 +28,5 @@ while True:
         conn.commit()
         cur.close()
         conn.close()
-    time.sleep(10 - (time.time() - start))
+    time.sleep(60 - (time.time() - start))
         
