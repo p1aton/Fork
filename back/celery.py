485804c6db8ade6806e2d8b44e21cce6b66ffccf
artemrychko <32412425+artemrychko@users.noreply.github.com>
2018-01-23 21:29:28 +0300

    Create celery.py

---
+++ back/celery.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, unicode_literals
+from celery import Celery
+
+app = Celery('proj',
+             broker='amqp://',
+             include=['back.tasks'])
+
+app.conf.update(
+    result_expires=3600,
+)
+
+if __name__ == '__main__':
+    app.start()
