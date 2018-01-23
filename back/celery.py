72e161eb0bade1d100812270d1c45f5c5c4f5abe
ivigamberdiev <ivigamberdiev@gmail.com>
2018-01-23 22:02:48 +0300

    Update celery.py

--- back/celery.py
+++ back/celery.py
@@ -1,13 +1,1 @@
-from __future__ import absolute_import, unicode_literals
-from celery import Celery
 
-app = Celery('proj',
-             broker='amqp://',
-             include=['back.tasks'])
-
-app.conf.update(
-    result_expires=3600,
-)
-
-if __name__ == '__main__':
-    app.start()
